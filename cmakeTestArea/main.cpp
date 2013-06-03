0b4494b11e5770fa2f6c7ed02084bb54fd725c80
Sam Mancill <cromambo@gmail.com>
2013-06-03 14:06:52 -0700

    Added section for testing cmake

---
+++ cmakeTestArea/main.cpp
@@ -0,0 +1,21 @@
+//============================================================================
+// Name         : main
+// Author       : {your name}
+// Version      : {Version}
+// Date         : 20130513  
+// License      : No License
+// Description  : To be added........
+//============================================================================
+
+#include <stdio.h>
+#include <iostream>
+using std::cout;
+using std::endl;
+using std::cin;
+
+int main(int argc, char* argv[])  
+{
+    cout << "hallo world" << endl;
+    return 0;
+	
+}
